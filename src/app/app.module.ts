e2e383b15a2355bafb89bc9466fe1e9c3c36efc5
Ankita <ankitamahajan329@gmail.com>
2021-03-23 22:33:56 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { HiComponent } from './hi/hi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    HiComponent
   ],
   imports: [
     BrowserModule,
